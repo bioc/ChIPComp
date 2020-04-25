1309a3c5f7eabb02d12e6038eae1e072b457fc93
lichen <lichen@lichens-MacBook-Air.local>
2020-04-25 16:05:59 -0400

    test_ChIPComp.R commit message

--- inst/unitTests/test_ChIPComp.R
+++ inst/unitTests/test_ChIPComp.R
@@ -1,5 +1,3 @@
-data(seqData)
-
 
 test.makeConf<-function(){
    	x=makeConf(system.file("extdata", "conf.csv", package="ChIPComp"))
@@ -22,21 +20,24 @@ test.makeCountSet<-function(){
 		ctReads=system.file("extdata",c("Helas3.ct.bed","Helas3.ct.bed","K562.ct.bed","K562.ct.bed"),package="ChIPComp"),
 		peaks=system.file("extdata",c("Helas3.peak.bed","Helas3.peak.bed","K562.peak.bed","K562.peak.bed"),package="ChIPComp")
 	)
+	conf$condition=factor(conf$condition)
+	conf$factor=factor(conf$factor)
 	design=as.data.frame(lapply(conf[,c("condition","factor")],as.numeric))-1
 	design=as.data.frame(model.matrix(~condition,design))
  	x=makeCountSet(conf,design,filetype="bed", species="hg19",binsize=1000)
 	checkIdentical("ChIPComp",as.character(class(x)))
-    checkEquals(length(x),2)
+  checkEquals(length(x),2)
 	checkEquals(ncol(x$db),12)
 	checkEquals(ncol(x$design),2)
-    checkEquals(names(x), c("db","design"))
-    checkEquals(names(x$db),c("chr","start","end","ip_c0_r1","ip_c0_r2","ip_c1_r1","ip_c1_r2","ct_c0_r1","ct_c0_r2","ct_c1_r1","ct_c1_r2","commonPeak"))
+  checkEquals(names(x), c("db","design"))
+  checkEquals(names(x$db),c("chr","start","end","ip_c0_r1","ip_c0_r2","ip_c1_r1","ip_c1_r2","ct_c0_r1","ct_c0_r2","ct_c1_r1","ct_c1_r2","commonPeak"))
 	checkException(makeCountSet(conf,design,filetype="bed", species="hg18"))
 }
  
  
  
  test.ChIPComp<-function(){
+   data(seqData)
  	x=ChIPComp(seqData)
     checkIdentical("ChIPComp",as.character(class(x)))
     checkEquals(length(x),2)
